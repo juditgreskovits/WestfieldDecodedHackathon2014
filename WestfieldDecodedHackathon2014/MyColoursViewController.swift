b95f85d371e2082efdaeb783bd46aa6a11b121e2
Judit Greskovits <juditgreskovits@gmail.com>
2014-09-20 14:53:58 +0100

    added colours screen

---
+++ WestfieldDecodedHackathon2014/MyColoursViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyColoursViewController.swift
+//  WestfieldDecodedHackathon2014
+//
+//  Created by JUDIT GRESKOVITS on 20/09/2014.
+//  Copyright (c) 2014 JUDIT GRESKOVITS. All rights reserved.
+//
+
+import UIKit
+
+class MyColoursViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
