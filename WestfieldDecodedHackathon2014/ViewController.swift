6ff6265d2a5180448d16f1a0d545b6a103f2191d
Judit Greskovits <juditgreskovits@gmail.com>
2014-09-20 14:31:41 +0100

    Initial Commit

---
+++ WestfieldDecodedHackathon2014/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WestfieldDecodedHackathon2014
+//
+//  Created by JUDIT GRESKOVITS on 20/09/2014.
+//  Copyright (c) 2014 JUDIT GRESKOVITS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
